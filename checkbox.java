02ecafd635adcbae3dd7385002a16d3c7276ac28
Pranav ambre <148484286+Pranavambre1@users.noreply.github.com>
2024-10-16 20:06:32 +0530

    Add files via upload

---
+++ checkbox.java
@@ -0,0 +1,65 @@
+/*
+* <applet code=checkbox width=300 height=100>
+* </applet>
+*/
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+public class checkbox extends Applet
+{
+
+MyCheckbox myCheckbox1, myCheckbox2, myCheckbox3,myCheckbox4;
+public void init()
+{
+
+myCheckbox1 = new MyCheckbox("Item 1");
+add(myCheckbox1);
+myCheckbox2 = new MyCheckbox("Item 2");
+add(myCheckbox2);
+myCheckbox3 = new MyCheckbox("Item 3");
+add(myCheckbox3);
+myCheckbox4 = new MyCheckbox("Item 4");
+add(myCheckbox4);
+}
+class MyCheckbox extends Checkbox
+{
+public MyCheckbox(String label)
+{
+super(label);
+enableEvents(AWTEvent.ITEM_EVENT_MASK);
+}
+protected void processItemEvent(ItemEvent ie)
+{
+showStatus("Checkbox name " + getLabel() + getState());
+super.processItemEvent(ie);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ listcontrol.java
@@ -0,0 +1,42 @@
+
+/*
+* <applet code="listcontrol.class" width=800 height=800>
+* </applet>
+*/
+
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+public class listcontrol extends Applet
+{
+MyList list;
+static String s;
+public void init()
+{
+list = new MyList();
+list.add("Red");
+list.add("Green");
+list.add("Blue");
+add(list);
+}
+
+public void paint(Graphics g)
+{
+g.drawString(s,30,60);
+}
+class MyList extends List
+{
+public MyList()
+{
+enableEvents(AWTEvent.ACTION_EVENT_MASK);
+}
+protected void processActionEvent(ActionEvent ie)
+{
+s="Your selection is "+ie.getActionCommand();
+showStatus(s);
+
+super.processActionEvent(ie);
+}
+}
+}
